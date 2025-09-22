30675f410a9b32adaedf673082bf10d08044caf8
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2025-09-22 10:17:38 +0300

    Update play.py

--- MatrixMusic/plugins/play/play.py
+++ MatrixMusic/plugins/play/play.py
@@ -5,7 +5,7 @@
 import random
 import string
 
-from ZelzalMusic.plugins.play.filters import command
+from MatrixMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
@@ -669,3 +669,4 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+
