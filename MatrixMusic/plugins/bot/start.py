120a5a4fe8d93d8f173610c8a88607fe57ffb4a3
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2025-09-22 17:09:33 +0300

    Update start.py

--- MatrixMusic/plugins/bot/start.py
+++ MatrixMusic/plugins/bot/start.py
@@ -1,29 +1,116 @@
+
 import time
 
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, Message
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from MatrixMusic import app
 from MatrixMusic.misc import _boot_
-from MatrixMusic.utils.database import add_served_chat, add_served_user
+from MatrixMusic.plugins.sudo.sudoers import sudoers_list
+from MatrixMusic.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
 from MatrixMusic.utils.decorators.language import LanguageStart
 from MatrixMusic.utils.formatters import get_readable_time
-from MatrixMusic.utils.inline import start_panel
+from MatrixMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/shahmplus"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/Shahmplus"),
+        ],
+    ]
+    return buttons
 
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    await message.reply_photo(
-        photo='MatrixMusic/plugins/bot/matrix.png',
-        caption=_["start_2"].format(app.mention),
-        reply_markup=InlineKeyboardMarkup(start_panel(_)),
-        reply_to_message_id=message.id
-    )
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+        if name[0:4] == "help":
+            keyboard = help_pannel(_)
+            return await message.reply_photo(
+                photo='MatrixMusic/plugins/bot/matrix.png',
+                caption=_["help_1"].format(config.SUPPORT_CHANNEL),
+                reply_markup=keyboard,
+            )
+        if name[0:3] == "sud":
+            await sudoers_list(client=client, message=message, _=_)
+            return
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = _["start_6"].format(
+                title, duration, views, published, channellink, channel, app.mention
+            )
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                chat_id=message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                reply_markup=key,
+            )
+    else:
+        # استخدام الأزرار الجديدة للخاص
+        out = private_panel(_)
+        await message.reply_photo(
+            photo='MatrixMusic/plugins/bot/matrix.png',
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
@@ -35,7 +122,5 @@ async def start_gp(client, message: Message, _):
         photo='MatrixMusic/plugins/bot/matrix.png',
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
-        reply_to_message_id=message.id
     )
-    await add_served_chat(message.chat.id)
-
+    return await add_served_chat(message.chat.id)
